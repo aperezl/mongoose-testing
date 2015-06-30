61cba82a3daf402dc4a6c8f4e9541a9acaded25c
Antonio Manuel Pérez López <aperezl@gmail.com>
2015-06-30 02:04:54 +0200

    Autocommit

--- test/models.js
+++ test/models.js
@@ -3,19 +3,24 @@
 var utils = require('./utils');
 var should = require('should');
 var User = require('../models/user');
+var userCtrl = require('../controllers/users');
+
 
 describe('Users: models', function() {
   describe('#create()', function() {
     it('should create a new User', function(done) {
-      var u = {
+      var u = new User({
         username: 'demo1',
         password: 'demo1'
-      };
+      });
 
-      User.create(u, function(err, createdUser) {
-        should.not.exits(err);
+      u.save(function(err, createdUser) {
+        should.not.exists(err); 
+        createdUser.username.should.equal('demo1');
+	//createdUser.password.should.equal('$2a$05$PIn6I2/L/M5JeaLnsYRyI.ZzAPboOZPK.fFkqyGg59TyYe8HTNwvy');  
         done();      
       });
+
     });
   });
 });
